7a0fea524d3912aba45e0edadf1d7183c7518379
Dhvanan Trivedi <dtrivedi@benelog.com>
2020-06-01 19:16:28 +0530

    added all test cases as per Kata-1, most of all should fail. Improved naming convention

--- src/test/java/com/dhvanan/StringCalculatorTest.java
+++ src/test/java/com/dhvanan/StringCalculatorTest.java
@@ -10,17 +10,77 @@ import org.junit.jupiter.api.Test;
 public class StringCalculatorTest {
 	
     @Test
-    public void add_ShouldWorkWithEmptyString() {
+    public void add_EmptyString_ShouldReturnZero() {
     	final StringCalculator calculator = new StringCalculator();
-    	final int add = calculator.add("");
-    	Assertions.assertTrue(add == 0);
+    	Assertions.assertEquals(0, calculator.add(""));
     }
     
     @Test
-    public void add_ShouldWorkWith2SimpleNumbers() {
+    public void add_2SimpleNumbers_CalculatedResult() {
     	final StringCalculator calculator = new StringCalculator();
-    	final int add = calculator.add("1,2");
-    	Assertions.assertTrue(add == 3);
+    	Assertions.assertEquals(3, calculator.add("1,2"));
+    }
+    
+    @Test
+    public void add_RandomAmountOfNumbers_CalculatedResults() {
+    	final StringCalculator calculator = new StringCalculator();
+    	Assertions.assertEquals(6, calculator.add("1,2,3"));
+    	Assertions.assertEquals(25, calculator.add("1,2,3,4,5,10"));
+    	Assertions.assertEquals(51, calculator.add("1,2,3,4,5,10,11,15"));
+    }
+    
+    @Test
+    public void add_NewLineBetweenNumbers_ConsiderNewLineAsAnotherDelimiter() {
+    	final StringCalculator calculator = new StringCalculator();
+    	Assertions.assertEquals(6, calculator.add("1\n2,3"));
+    }
+    
+    
+    @Test
+    public void add_CustomOptionalDelimiterSuppliedAtTheStartOfTheString_WorkAsDelimiterIfSupplied() {
+    	final StringCalculator calculator = new StringCalculator();
+    	Assertions.assertEquals(3, calculator.add("//;\n1;2"));
+    	Assertions.assertEquals(6, calculator.add("|\n2|4"));
+    	Assertions.assertEquals(3, calculator.add("1,2"));
+    }
+    
+    @Test
+    public void add_NegativeNumbers_ShouldThrowAndException() {
+    	
+    	final Throwable exception = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                	final StringCalculator calculator = new StringCalculator();
+                	calculator.add("-1,2");
+                }
+        );
+     
+    	Assertions.assertEquals("negatives not allowed - [-1] is supplied", exception.getMessage());
+    }
+    
+    @Test
+    public void add_NumberBiggerThan1000IsAdded_NumberBiggerThan1000ShouldBeIgnored() {
+    	final StringCalculator calculator = new StringCalculator();
+    	Assertions.assertEquals(3, calculator.add("1,2,1003"));
+    }
+    
+    @Test
+    public void add_DelimitersOfAnyLength_ShouldCalculateBasedOnTheDelimiters() {
+    	final StringCalculator calculator = new StringCalculator();
+    	Assertions.assertEquals(6, calculator.add("//[***]\n1***2***3"));
+    	Assertions.assertEquals(10, calculator.add("//[****]\n2****3****5"));
+    }
+    
+    @Test
+    public void add_MultipleDelimitersAreSupplied_AllOfTheSuppliedDelimitersShouldBeConsidered() {
+    	final StringCalculator calculator = new StringCalculator();
+    	Assertions.assertEquals(6, calculator.add("//[*][%]\n1*2%3"));
+    }
+    
+    
+    @Test
+    public void add_MultipleDelimitersOfAnyLenghtAreSupplied_AllOfTheSuppliedDelimitersShouldBeConsidered() {
+    	final StringCalculator calculator = new StringCalculator();
+    	Assertions.assertEquals(6, calculator.add("//[**][%%]\n1**2%%3"));
     }
     
     
