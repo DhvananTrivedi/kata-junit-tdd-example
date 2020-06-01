d5420282ca6e48bb672d354193c5ad15e78ba042
Dhvanan Trivedi <dtrivedi@benelog.com>
2020-06-01 19:45:36 +0530

    implemented basic StringCalculator that adds with "," delimiters - see StringCalculator.java

--- src/main/java/com/dhvanan/StringCalculator.java
+++ src/main/java/com/dhvanan/StringCalculator.java
@@ -1,9 +1,24 @@
 package com.dhvanan;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StringCalculator {
-	
+
+	private static final Logger LOG = LoggerFactory.getLogger(StringCalculator.class);
+
 	public int add(String numbers) {
-		return 0;
+		final List<String> numbersList = Arrays.asList(numbers.split(","));
+		
+		final int sum = numbersList.stream()
+									.mapToInt(Integer::parseInt)
+									.sum();
+		
+		LOG.info("addition of [{}] = {}", numbers, sum);
+		return sum;
 	}
 
 }
